7cedee2833b4aa231b801d58b5077bc84d5c2a39
GubinAlexander <gubinalexander0598@gmail.com>
2017-03-31 23:34:07 +0300

    simple script

---
+++ First/First.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+import sys
+def main():
+ print "Hello, World!"
+ print "This is Alexander".format(name=sys.argv[0])
+if __name__ == "__main__":
+ main()
